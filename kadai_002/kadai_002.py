50701d0b28f0072ad6cca42302a0d07acba41f6b
hayato634 <154353972+hayato634@users.noreply.github.com>
2024-06-09 17:01:07 +0900

    Add files via upload

--- kadai_002/kadai_002.py
+++ kadai_002/kadai_002.py
@@ -61,9 +61,21 @@ stopwords_list = stopwords_text.split("\r\n")
 stopwords_list = [word for word in stopwords_list if word]
 print(stopwords_list)
 
-# ストップワードによる除去
-delete_stop_words = [word for word in main_text if word not in stopwords_list]
-print(delete_stop_words)
+
+# 最初の1文（近頃は～思っています。）について、手動で品詞ごとに区切り、ストップワードを除去
+
+split_text_list =  ['近頃','は','大分','方々','の','雑誌','から','談話','を','しろしろ','と','責められ','て',\
+    '、','頭','が','がらん胴','に','なった','から','、','当分','品切れ','の','看板','でも'\
+        ,'懸けたい','くらい','に','思って','います','。']
+result_text_list = list()
+for split_text in split_text_list:
+  if split_text not in stopwords_list:
+    result_text_list.append(split_text)
+
+print(result_text_list)
+
+
+
 
 
 
